32322eb3b642a85b53a367adedcb91a46469b1f0
xhb <xb864083144@outlook.com>
2018-11-14 11:43:19 +0800

    [update] 优化代码结构

--- src/components/vue-photo-zoom-pro.vue
+++ src/components/vue-photo-zoom-pro.vue
@@ -1,220 +1,218 @@
 <template>
-  <div v-if="url" class="pic-img">
-    <div class="img-container" @mousemove="!moveEvent && mouseMove($event)"
-         @mouseleave="!leaveEvent &&  mouseLeave($event)">
-      <img ref="img" @load="imgLoaded" :src="url" style="width:100%"/>
-      <div v-if="!hideZoom && imgLoadedFlag" :class="['img-selector',{'circle':type === 'circle'}]"
-           :style="[imgSelectorSize,imgSelectorPosition,!outShow && imgSelectorBg ,!outShow && imgBgPosition]"></div>
-      <div v-if="outShow" v-show="!hideOutShow" class="img-out-show"
-           :style="[imgOutShowSize,imgSelectorBg,imgBgPosition]"></div>
+  <div class="pic-img">
+    <div class="img-container" @mousemove="!moveEvent && mouseMove($event)" @mouseleave="!leaveEvent && mouseLeave($event)">
+      <img ref="img" @load="imgLoaded" :src="url" style="width:100%"></img>
+      <div v-if="!hideZoom && imgLoadedFlag" :class="['img-selector',{'circle':type === 'circle'}]" :style="[imgSelectorSize,imgSelectorPosition,!outShow && imgSelectorBg ,!outShow && imgBgPosition]"> </div>
+      <div v-if="outShow" v-show="!hideOutShow" class="img-out-show" :style="[imgOutShowSize,imgSelectorBg,imgBgPosition]"></div>
     </div>
   </div>
 
 </template>
 <script>
-  export default {
-    name: 'vue-photo-zoom-pro',
-    data() {
-      return {
-        selector: {
-          width: 166,
-          halfWidth: 83,
-          top: 0,
-          left: 0,
-          bgTop: 0,
-          bgLeft: 0,
-          rightBound: 0,
-          bottomBound: 0,
-          absoluteLeft: 0,
-          absoluteTop: 0
-        },
-        imgInfo: {},
-        hideOutShow: true,
-        imgLoadedFlag: false,
-        screenWidth: document.body.clientWidth,
-        timer: null
-      };
-    },
-    props: {
-      url: String,
-      highUrl: String,
-      type: {
-        type: String,
-        default: 'square',
-        validator: function (value) {
-          return ['circle', 'square'].indexOf(value) !== -1;
-        }
+export default {
+  name: 'vue-photo-zoom-pro',
+  data() {
+    return {
+      selector: {
+        width: 166,
+        halfWidth: 83,
+        top: 0,
+        left: 0,
+        bgTop: 0,
+        bgLeft: 0,
+        rightBound: 0,
+        bottomBound: 0,
+        absoluteLeft: 0,
+        absoluteTop: 0
       },
-      scale: {
-        type: Number,
-        default: 3
-      },
-      moveEvent: {
-        type: [Object, MouseEvent],
-        default: null
-      },
-      leaveEvent: {
-        type: [Object, MouseEvent],
-        default: null
-      },
-      hideZoom: {
-        type: Boolean,
-        default: false
-      },
-      outShow: {
-        type: Boolean,
-        default: false
+      imgInfo: {},
+      $img:null,
+      hideOutShow: true,
+      imgLoadedFlag: false,
+      screenWidth: document.body.clientWidth,
+      timer: null
+    };
+  },
+  props: {
+    url: String,
+    highUrl: String,
+    type: {
+      type: String,
+      default: 'square',
+      validator: function(value) {
+        return ['circle', 'square'].indexOf(value) !== -1;
       }
     },
-    watch: {
-      moveEvent(e) {
-        this.mouseMove(e);
-      },
-      leaveEvent(e) {
-        this.mouseLeave(e);
-      },
-      url() {
-        this.imgLoadedFlag = false;
-      },
-      screenWidth(val) {
-        if (!this.timer) {
-          this.screenWidth = val;
-          this.timer = setTimeout(() => {
-            this.imgLoaded();
-            clearTimeout(this.timer);
-            this.timer = null;
-          }, 400);
-        }
-      }
+    scale: {
+      type: Number,
+      default: 3
     },
-    computed: {
-      imgSelectorPosition() {
-        let {top, left} = this.selector;
-        return {
-          top: `${top}px`,
-          left: `${left}px`
-        };
-      },
-      imgSelectorSize() {
-        let width = this.selector.width;
-        return {
-          width: `${width}px`,
-          height: `${width}px`
-        };
-      },
-      imgOutShowSize() {
-        let {
-          scale,
-          selector: {width}
-        } = this;
-        return {
-          width: `${width * scale}px`,
-          height: `${width * scale}px`
-        };
-      },
-      imgSelectorBg() {
-        let {
-          scale,
-          url,
-          highUrl,
-          imgInfo: {height, width}
-        } = this;
-        return {
-          backgroundImage: `url(${highUrl || url})`,
-          backgroundSize: `${width * scale}px ${height * scale}px`
-        };
-      },
-      imgBgPosition() {
-        let {bgLeft, bgTop} = this.selector;
-        return {
-          backgroundPosition: `${bgLeft}px ${bgTop}px`
-        };
-      }
+    moveEvent: {
+      type: [Object, MouseEvent],
+      default: null
     },
-    methods: {
-      imgLoaded() {
-        let imgInfo = this.$refs['img'].getBoundingClientRect();
-        if (JSON.stringify(this.imgInfo) == JSON.stringify(imgInfo)) {
-          return;
-        }
-        this.imgLoadedFlag = true;
-        let {width, height, left, top} = (this.imgInfo = imgInfo);
-        let selector = this.selector;
-        let {width: selectorWidth, halfWidth: selectorHalfWidth} = selector;
-        Object.assign(selector, {
-          absoluteLeft: left + selectorHalfWidth, // 选择器相对于文档左上角的位置
-          absoluteTop:
-            top + selectorHalfWidth + document.documentElement.scrollTop,
-          rightBound: width - selectorWidth, // 相对于图片左上角的边界值
-          bottomBound: height - selectorWidth
-        });
-      },
-      reset() {
-        Object.assign(this.selector, {
-          top: 0,
-          left: 0,
-          bgLeft: 0,
-          bgTop: 0
-        });
-      },
-      mouseMove(e) {
-        if (!this.hideZoom && this.imgLoadedFlag) {
+    leaveEvent: {
+      type: [Object, MouseEvent],
+      default: null
+    },
+    hideZoom: {
+      type: Boolean,
+      default: false
+    },
+    outShow: {
+      type: Boolean,
+      default: false
+    }
+  },
+  watch: {
+    moveEvent(e) {
+      this.mouseMove(e);
+    },
+    leaveEvent(e) {
+      this.mouseLeave(e);
+    },
+    url() {
+      this.imgLoadedFlag = false;
+    },
+    screenWidth(val) {
+      if (!this.timer) {
+        this.screenWidth = val;
+        this.timer = setTimeout(() => {
           this.imgLoaded();
-          let {pageX, pageY} = e;
-          let {scale, selector} = this;
-          let {
-            halfWidth,
-            absoluteLeft,
-            absoluteTop,
-            rightBound,
-            bottomBound
-          } = selector;
-          let x = pageX - absoluteLeft; // 选择器的x坐标 相对于图片
-          let y = pageY - absoluteTop; // 选择器的y坐标
-          if (this.outShow) {
-            halfWidth = 0;
-            this.hideOutShow = false;
-          }
-          Object.assign(selector, {
-            top: y > 0 ? (y < bottomBound ? y : bottomBound) : 0, // 超出边界重置
-            left: x > 0 ? (x < rightBound ? x : rightBound) : 0,
-            bgLeft: halfWidth - (halfWidth + x) * scale, // 选择器图片的坐标位置
-            bgTop: halfWidth - (halfWidth + y) * scale
-          });
-        }
-      },
-      mouseLeave() {
-        if (this.outShow) {
-          this.hideOutShow = true;
-        }
+          clearTimeout(this.timer);
+          this.timer = null;
+        }, 400);
       }
     }
-  };
+  },
+  computed: {
+    imgSelectorPosition() {
+      let { top, left } = this.selector;
+      return {
+        top: `${top}px`,
+        left: `${left}px`
+      };
+    },
+    imgSelectorSize() {
+      let width = this.selector.width;
+      return {
+        width: `${width}px`,
+        height: `${width}px`
+      };
+    },
+    imgOutShowSize() {
+      let {
+        scale,
+        selector: { width }
+      } = this;
+      return {
+        width: `${width * scale}px`,
+        height: `${width * scale}px`
+      };
+    },
+    imgSelectorBg() {
+      let {
+        scale,
+        url,
+        highUrl,
+        imgInfo: { height, width }
+      } = this;
+      return {
+        backgroundImage: `url(${highUrl || url})`,
+        backgroundSize: `${width * scale}px ${height * scale}px`
+      };
+    },
+    imgBgPosition() {
+      let { bgLeft, bgTop } = this.selector;
+      return {
+        backgroundPosition: `${bgLeft}px ${bgTop}px`
+      };
+    }
+  },
+  mounted() {
+    this.$img = this.$refs['img'];
+  },
+  methods: {
+    imgLoaded() {
+      let imgInfo = this.$img.getBoundingClientRect();
+      if (JSON.stringify(this.imgInfo) == JSON.stringify(imgInfo)) {
+        return;
+      }
+      this.imgLoadedFlag = true;
+      let { width, height, left, top } = (this.imgInfo = imgInfo);
+      let selector = this.selector;
+      let { width: selectorWidth, halfWidth: selectorHalfWidth } = selector;
+      let { scrollLeft, scrollTop } = document.documentElement;
+      selector.rightBound = width - selectorWidth;
+      selector.bottomBound = height - selectorWidth;
+      selector.absoluteLeft = left + selectorHalfWidth + scrollLeft;
+      selector.absoluteTop = top + selectorHalfWidth + scrollTop;
+    },
+    reset() {
+      Object.assign(this.selector, {
+        top: 0,
+        left: 0,
+        bgLeft: 0,
+        bgTop: 0
+      });
+    },
+    mouseMove(e) {
+      if (!this.hideZoom && this.imgLoadedFlag) {
+        this.imgLoaded();
+        let { pageX, pageY } = e;
+        let { scale, selector } = this;
+        let {
+          halfWidth,
+          absoluteLeft,
+          absoluteTop,
+          rightBound,
+          bottomBound
+        } = selector;
+        let x = pageX - absoluteLeft; // 选择器的x坐标 相对于图片
+        let y = pageY - absoluteTop; // 选择器的y坐标
+        if (this.outShow) {
+          halfWidth = 0;
+          this.hideOutShow = false;
+        }
+        selector.top = y > 0 ? (y < bottomBound ? y : bottomBound) : 0;
+        selector.left = x > 0 ? (x < rightBound ? x : rightBound) : 0;
+        selector.bgLeft = halfWidth - (halfWidth + x) * scale; // 选择器图片的坐标位置
+        selector.bgTop = halfWidth - (halfWidth + y) * scale;
+      }
+    },
+    mouseLeave() {
+      if (this.outShow) {
+        this.hideOutShow = true;
+      }
+    }
+  }
+};
 </script>
 
 <style scoped>
-  .img-container {
-    position: relative;
-  }
+.img-container {
+  position: relative;
+}
 
-  .img-selector {
-    position: absolute;
-    background-color: rgba(0, 0, 0, 0.6);
-    background-repeat: no-repeat;
-    cursor: crosshair;
-    border: 1px solid rgba(0, 0, 0, 0.1);
-  }
+.img-selector {
+  background-color: rgba(0, 0, 0, 0.6);
+  position: absolute;
+  background-repeat: no-repeat;
+  cursor: crosshair;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+}
 
-  .img-selector.circle {
-    border-radius: 50%;
-  }
+.img-selector.circle {
+  border-radius: 50%;
+}
 
-  .img-out-show {
-    position: absolute;
-    top: 0;
-    right: 0;
-    background-repeat: no-repeat;
-    transform: translate(100%, 0);
-    border: 1px solid rgba(0, 0, 0, 0.1);
-  }
+.img-out-show {
+  position: absolute;
+  top: 0;
+  right: 0;
+  background-repeat: no-repeat;
+  transform: translate(100%, 0);
+  border: 1px solid rgba(0, 0, 0, 0.1);
+}
 </style>
+
