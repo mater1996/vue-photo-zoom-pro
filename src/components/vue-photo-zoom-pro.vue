77634aeadfcd63bcd97ec3ab9ddb979ec426e4db
xhb <xb864083144@outlook.com>
2018-11-16 19:30:00 +0800

    [update] 优化代码结构

--- src/components/vue-photo-zoom-pro.vue
+++ src/components/vue-photo-zoom-pro.vue
@@ -2,8 +2,12 @@
   <div class="pic-img">
     <div class="img-container" @mousemove="!moveEvent && mouseMove($event)" @mouseleave="!leaveEvent && mouseLeave($event)">
       <img ref="img" @load="imgLoaded" :src="url" style="width:100%"></img>
-      <div v-if="!hideZoom && imgLoadedFlag" :class="['img-selector',{'circle':type === 'circle'}]" :style="[imgSelectorSize,imgSelectorPosition,!outShow && imgSelectorBg ,!outShow && imgBgPosition]"> </div>
-      <div v-if="outShow" v-show="!hideOutShow" class="img-out-show" :style="[imgOutShowSize,imgSelectorBg,imgBgPosition]"></div>
+      <div v-if="!hideZoom && imgLoadedFlag &&!hideSelector" :class="['img-selector',{'circle':type === 'circle'}]" :style="[imgSelectorSize,imgSelectorPosition,!outShow && imgSelectorBg ,!outShow && imgBgPosition]">
+
+      </div>
+      <div v-if="outShow" v-show="!hideOutShow" class="img-out-show" :style="[imgOutShowSize,imgSelectorBg,imgBgPosition]">
+        <div class="img-selector-point"></div>
+      </div>
     </div>
   </div>
 
@@ -26,11 +30,12 @@ export default {
         absoluteTop: 0
       },
       imgInfo: {},
-      $img:null,
+      $img: null,
       hideOutShow: true,
       imgLoadedFlag: false,
       screenWidth: document.body.clientWidth,
-      timer: null
+      timer: null,
+      hideSelector: false
     };
   },
   props: {
@@ -135,10 +140,11 @@ export default {
   methods: {
     imgLoaded() {
       let imgInfo = this.$img.getBoundingClientRect();
+      this.imgLoadedFlag = true;
+      this.$emit('created', this.imgInfo);
       if (JSON.stringify(this.imgInfo) == JSON.stringify(imgInfo)) {
         return;
       }
-      this.imgLoadedFlag = true;
       let { width, height, left, top } = (this.imgInfo = imgInfo);
       let selector = this.selector;
       let { width: selectorWidth, halfWidth: selectorHalfWidth } = selector;
@@ -174,6 +180,7 @@ export default {
           halfWidth = 0;
           this.hideOutShow = false;
         }
+        this.hideSelector = false;
         selector.top = y > 0 ? (y < bottomBound ? y : bottomBound) : 0;
         selector.left = x > 0 ? (x < rightBound ? x : rightBound) : 0;
         selector.bgLeft = halfWidth - (halfWidth + x) * scale; // 选择器图片的坐标位置
@@ -181,6 +188,7 @@ export default {
       }
     },
     mouseLeave() {
+      this.hideSelector = true;
       if (this.outShow) {
         this.hideOutShow = true;
       }
@@ -213,6 +221,40 @@ export default {
   background-repeat: no-repeat;
   transform: translate(100%, 0);
   border: 1px solid rgba(0, 0, 0, 0.1);
+}
+
+.img-selector-point {
+  position: absolute;
+  width: 5px;
+  height: 5px;
+  background-color: black;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.img-out-show::after {
+  position: absolute;
+  box-sizing: border-box;
+  content: '';
+  width: 1px;
+  border: 1px dashed rgba(0, 0, 0, 0.36);
+  top: 0;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.img-out-show::before {
+  position: absolute;
+  box-sizing: border-box;
+  content: '';
+  height: 1px;
+  border: 1px dashed rgba(0, 0, 0, 0.36);
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
 }
 </style>
 
