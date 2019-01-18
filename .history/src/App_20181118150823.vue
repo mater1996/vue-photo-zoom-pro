ce43a17463c5b6c69ac993c44f10903b28be6767
xhb <xb864083144@outlook.com>
2019-01-18 09:55:17 +0800

    [update] add selector width and style

---
+++ .history/src/App_20181118150823.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <div style="width:60%">
+      <vue-photo-zoom-pro :url="url" :type="type" :scale="scale" :out-show="showType"></vue-photo-zoom-pro>
+    </div>
+    <p>当前放大倍数：{{scale}}</p>
+    <button @click="addScale">增加放大倍数</button>
+    <button @click="subScale">减小放大倍数</button>
+    <button @click="changeImg">更换图片</button>
+    <button @click="changeType">更换放大镜类型</button>
+    <button @click="changeShowType">更换放大方式</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      url: '',
+      index: 0,
+      scale: 2,
+      type: 'circle',
+      showType: false,
+      imgList: [
+        'https://bpic.588ku.com/illus_water_img/18/07/30/f3c7060bc28216271dc8c4630b288331.jpg!/watermark/url/L3dhdGVyL3dhdGVyX2JhY2tfNDAwXzIwMC5wbmc=/repeat/true',
+        'https://bpic.588ku.com/illus_water_img/18/08/21/64d2b3b02352b00dd668b705fd8af276.jpg!/watermark/url/L3dhdGVyL3dhdGVyX2JhY2tfNDAwXzIwMC5wbmc=/repeat/true',
+        'https://bpic.588ku.com/illus_water_img/18/08/18/472f21d353b64c95b9994be6ed3b7274.jpg!/watermark/url/L3dhdGVyL3dhdGVyX2JhY2tfNDAwXzIwMC5wbmc=/repeat/true'
+      ]
+    };
+  },
+  created() {
+    this.url = this.imgList[this.index];
+  },
+  methods: {
+    addScale() {
+      this.scale++;
+    },
+    subScale() {
+      this.scale--;
+    },
+    changeType() {
+      this.type = this.type === 'circle' ? 'square' : 'circle';
+    },
+    changeImg() {
+      this.index = this.index > this.imgList.length - 2 ? -1 : this.index;
+      this.url = this.imgList[++this.index];
+    },
+    changeShowType() {
+      this.showType = !this.showType;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ .history/src/main_20190118090118.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import vuePhotoZoomPro from '../dist/vue-photo-zoom-pro'
+
+Vue.use(vuePhotoZoomPro)
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
