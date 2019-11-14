bed76f240e8d1dd2da79e29b114e370b2c326e41
🦄 Mater <baixh@rd.netease.com>
2019-11-14 18:38:18 +0800

    [update] 修改示例布局

--- src/App.vue
+++ src/App.vue
@@ -7,36 +7,57 @@
     <div style="width:60%;display:inline-block">
       <vue-photo-zoom-pro
         :width="width"
+        :height="height"
         :url="url"
         :type="type"
         :scale="scale"
         :out-zoom="showType"
-        :zoom-style="{'background-color': taobao ? 'transparent' : 'rgba(0,0,0,0.6)'}"
+        :zoom-style="{
+          'background-color': taobao ? 'transparent' : 'rgba(0,0,0,0.6)'
+        }"
       >
         <template v-if="taobao" slot="header">
           <div class="ks-imagezoom-wrap"></div>
         </template>
       </vue-photo-zoom-pro>
     </div>
-    <p>当前放大倍数：{{ scale }}</p>
-    <p>当前放大镜宽度：{{ width }}</p>
-    <button @click="addSelectorWidth">
-      增加放大器宽度(add width of magnifying glass)
-    </button>
-    <button @click="subSelectorWidth">
-      减小放大器宽度(cut width of magnifying glass)
-    </button>
-    <button @click="changeImgPosition = !changeImgPosition">
-      改变图片位置(change position of image)
-    </button>
-    <button @click="addScale">增加放大倍数(add scale)</button>
-    <button @click="subScale">减小放大倍数(cut scale)</button>
-    <button @click="changeImg">更换图片(change img url)</button>
-    <button @click="changeType">
-      更换放大镜类型(change type of magnifying glass)
-    </button>
-    <button @click="changeShowType">更换放大方式(change type of scale)</button>
-    <button @click="changeToTaobao">类淘宝放大镜</button>
+    <div style="width:38%;display:inline-block;vertical-align:top;">
+      <p>当前放大倍数：{{ scale }}</p>
+      <p>当前放大镜宽度：{{ width }}</p>
+      <button style="margin-top:8px" @click="addSelectorWidth">
+        增加放大器宽度(add width)
+      </button>
+      <button style="margin-top:8px" @click="subSelectorWidth">
+        减小放大器宽度(cut width)
+      </button>
+      <button
+        style="margin-top:8px"
+        @click="changeImgPosition = !changeImgPosition"
+      >
+        改变图片位置(change position)
+      </button>
+      <button style="margin-top:8px" @click="addScale">
+        增加放大倍数(add scale)
+      </button>
+      <button style="margin-top:8px" @click="subScale">
+        减小放大倍数(cut scale)
+      </button>
+      <button style="margin-top:8px" @click="changeImg">
+        更换图片(change img url)
+      </button>
+      <button style="margin-top:8px" @click="changeType">
+        更换放大镜类型(change type)
+      </button>
+      <button style="margin-top:8px" @click="changeShowType">
+        更换放大方式(change type of scale)
+      </button>
+      <button style="margin-top:8px" @click="changeToTaobao">
+        类淘宝放大镜
+      </button>
+      <button style="margin-top:8px" @click="changeToMultipleWidth">
+        高宽不同
+      </button>
+    </div>
   </div>
 </template>
 
@@ -57,7 +78,8 @@ export default {
         "https://bpic.588ku.com/illus_water_img/18/08/21/64d2b3b02352b00dd668b705fd8af276.jpg!/watermark/url/L3dhdGVyL3dhdGVyX2JhY2tfNDAwXzIwMC5wbmc=/repeat/true",
         "https://bpic.588ku.com/illus_water_img/18/08/18/472f21d353b64c95b9994be6ed3b7274.jpg!/watermark/url/L3dhdGVyL3dhdGVyX2JhY2tfNDAwXzIwMC5wbmc=/repeat/true"
       ],
-      taobao: false
+      taobao: false,
+      height: -1
     };
   },
   created() {
@@ -86,9 +108,12 @@ export default {
     changeShowType() {
       this.showType = !this.showType;
     },
-    changeToTaobao(){
-      this.taobao = !this.taobao
-      this.showType = true
+    changeToTaobao() {
+      this.taobao = !this.taobao;
+      this.showType = true;
+    },
+    changeToMultipleWidth() {
+      this.height = this.height === -1 ? 99 : -1;
     }
   }
 };
