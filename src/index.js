5a0d46083b6c6c8cd1a0579609b7b02574728b34
Mater <bxh8640@gmail.com>
2021-09-01 09:57:09 +0800

    fix: jsconfig Root file specified for compilation error

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/*
+ * @author: Mater
+ * @Email: bxh8640@gmail.com
+ * @Date: 2021-09-01 09:53:55
+ * @Description:
+ */
+import vuePhotoZoomPro from './vue-photo-zoom-pro.vue'
+
+export default vuePhotoZoomPro
