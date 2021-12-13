b74789a61298d38d508b7843f5244e0e1a14aedb
Mater <bxh8640@gmail.com>
2021-12-13 10:20:36 +0800

    fix: update example

--- example/image/image.js
+++ example/image/image.js
@@ -2,18 +2,10 @@ new Vue({
   el: '#demo',
   data: function() {
     return {
-      scale: 2,
-      rotate: 0
+      scale: 2
     }
   },
   components: {
-    vuePhotoZoomPro: VuePhotoZoomPro,
-    canvasPreview: VuePhotoZoomProPluginCanvas.CanvasPreview,
-    canvasZoomer: VuePhotoZoomProPluginCanvas.CanvasZoomer
-  },
-  methods: {
-    handleRotate: function() {
-      this.rotate += 90
-    }
-  },
+    vuePhotoZoomPro: VuePhotoZoomPro
+  }
 })
