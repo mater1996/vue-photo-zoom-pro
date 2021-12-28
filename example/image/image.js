e901c74d434fad53e01d98faf5f5cdaf9c633c7c
Mater <bxh8640@gmail.com>
2021-12-28 16:07:28 +0800

    chore: add dat.gui to image example

--- example/image/image.js
+++ example/image/image.js
@@ -1,11 +1,40 @@
+console.log(window)
+
 new Vue({
   el: '#demo',
-  data: function() {
-    return {
-      scale: 2
-    }
-  },
   components: {
     vuePhotoZoomPro: VuePhotoZoomPro
+  },
+  data: function() {
+    return {
+      props: {
+        scale: 2,
+        width: 168,
+        height: -1,
+        url: './image.jpg',
+        highUrl: './image-high.jpg',
+        type: 'square',
+        selector: true,
+        outZoomer: false,
+        disabled: false,
+        mask: false,
+        maskColor: 'rgba(0,0,0,0.4)'
+      }
+    }
+  },
+  mounted: function() {
+    var props = this.props
+    var gui = new window.dat.GUI()
+    gui.add(props, 'scale', 1, 10)
+    gui.add(props, 'width', -1, 1000)
+    gui.add(props, 'height', -1, 562)
+    gui.add(props, 'url')
+    gui.add(props, 'highUrl')
+    gui.add(props, 'type').options(['circle', 'square'])
+    gui.add(props, 'selector')
+    gui.add(props, 'outZoomer')
+    gui.add(props, 'disabled')
+    gui.add(props, 'mask')
+    gui.addColor(props, 'maskColor')
   }
 })
